b5eb1cf3192828ab2ad9ea5e90129f9229119e07
VicDellaSala <113453944+VicDellaSala@users.noreply.github.com>
2024-04-01 02:20:41 +0200

    Comentarios extra

--- src/objetos/Estudiante.js
+++ src/objetos/Estudiante.js
@@ -6,6 +6,8 @@ export function Estudiante(name,email,number,picture,agrupaciones) {
     this.agrupaciones = agrupaciones;
   }
   
+// Crea un nuevo objeto de tipo Estudiante
+
 //   var estudiante = new Estudiante("Juan", 20, "Informática", "Universidad XYZ");
   
 //   console.log(estudiante);
